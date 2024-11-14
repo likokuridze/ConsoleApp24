6f2ef9711b84d9b78763ef7de03ba57c9ea9095f
LikoKuridze <lkuridze@credo.ge>
2024-11-15 01:26:04 +0400

    Add project files.

---
+++ ConsoleApp24/Fraction.cs
@@ -0,0 +1,56 @@
+﻿public class Fraction
+{
+    // მრიცხველი და მნიშვნელი
+    private int _numerator;
+    private int _denominator;
+
+
+    public Fraction(int numerator, int denominator)
+    {
+
+        // ვამოწმებთ მნიშვნელი არ არის თუ არა 0
+        if (denominator == 0)
+        {
+            Console.WriteLine("მნიშვნელი არ შეიძლება იყოს 0!");
+          
+            _denominator = 1;
+        }
+        else
+        {
+            _denominator = denominator;
+        }
+        _numerator = numerator;
+    }
+
+    // წილადის ტექსტად გამოტანა
+    public override string ToString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    // ორი წილადის შეკრება
+    public static Fraction operator +(Fraction a, Fraction b)
+    {
+        int newNumerator = (a._numerator * b._denominator) + (b._numerator * a._denominator);
+        int newDenominator = a._denominator * b._denominator;
+
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    // წილადების შედარება (ტოლია თუ არა)
+    public static bool operator ==(Fraction a, Fraction b)
+    {
+        return a._numerator == b._numerator && a._denominator == b._denominator;
+    }
+
+    // წილადების შედარება (არ არის ტოლი)
+    public static bool operator !=(Fraction a, Fraction b)
+    {
+        return !(a == b);
+    }
+}
+
+
+
+
+
---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main()
+    {
+        // პირველი წილადი 1/2
+        Fraction fraction1 = new Fraction(1, 2);
+        Console.WriteLine($"პირველი წილადი: {fraction1}");
+
+        // მეორე წილადი 1/3
+        Fraction fraction2 = new Fraction(1, 3);
+        Console.WriteLine($"მეორე წილადი: {fraction2}");
+
+        // წილადების შეკრება
+        Fraction sum = fraction1 + fraction2;
+        Console.WriteLine($"ჯამი: {sum}");
+
+        // წილადების შედარება
+        Console.WriteLine($"წილადები ტოლია? {fraction1 == fraction2}");
+        Console.WriteLine($"წილადები არ არის ტოლი? {fraction1 != fraction2}");
+    }
+}
